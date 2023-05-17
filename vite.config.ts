beab59709f7e8407cca24bb761f9a676cc79ed76
Reynaldo Quispe Sanchez <rquispe@makingsense.com>
2023-05-17 12:04:45 -0400

    feat: add declarations in distrubution build

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 import { resolve } from 'node:path'
@@ -6,13 +7,20 @@ import { resolve } from 'node:path'
 import * as packageJson from './package.json'
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
-  plugins: [tsconfigPaths()],
+  plugins: [
+    dts({
+      include: ['./src/components/'],
+      insertTypesEntry: true,
+    }),
+    tsconfigPaths(),
+  ],
   build: {
     lib: {
-      entry: resolve('src', 'components/index.ts'),
+      entry: resolve(__dirname, './src/components/index.ts'),
       name: 'MSWebComponentsLibrary',
       formats: ['es', 'umd'],
-      fileName: (format) => `ms-ui-web-components-library.${format}.js`,
+      fileName: 'ms-ui-web-components-library',
+      // fileName: (format) => `ms-ui-web-components-library.${format}.js`,
     },
     rollupOptions: {
       external: [...Object.keys(packageJson.peerDependencies), new RegExp(/\b\w*template\w*\b/gi)],
