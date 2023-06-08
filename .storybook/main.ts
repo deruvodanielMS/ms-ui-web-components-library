7d1d389502529f2c69dd982dae0f7752f5cb0218
Cecilia Mermoz <cmermoz@makingsense.com>
2023-06-08 13:12:55 -0300

    test

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,18 +1,20 @@
-import type { StorybookConfig } from "@storybook/web-components-vite";
+import type { StorybookConfig } from '@storybook/web-components-vite'
+
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
     '@storybook/addon-a11y',
   ],
   framework: {
-    name: "@storybook/web-components-vite",
+    name: '@storybook/web-components-vite',
     options: {},
   },
   docs: {
-    autodocs: "tag",
+    autodocs: 'tag',
   },
-};
-export default config;
+}
+
+export default config
--- src/components/button/my-element.ts
+++ src/components/button/my-element.ts
@@ -1,7 +1,9 @@
-import { LitElement, html } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
-import { myElementStyles } from './my-element.styles';
+import { LitElement, html } from 'lit'
+import { customElement, property } from 'lit/decorators.js'
 
+import { myElementStyles } from './my-element.styles'
+
+import '../../../libs/icomoon/style.css'
 /**
  * An example element.
  *
@@ -11,66 +13,89 @@ import { myElementStyles } from './my-element.styles';
 
 @customElement('my-element')
 export class MyElement extends LitElement {
-  static styles = [myElementStyles];
+  static styles = [myElementStyles]
 
   /**
    * The name to say "Hello" to.
    */
   @property({ type: String })
-  name = 'World';
+  name = 'World'
 
   /**
    * The number of times the button has been clicked.
    */
   @property({ type: Number })
-  count = 0;
+  count = 0
 
   /**
    * Whether to show the button as a submit button.
    */
   @property({ type: Boolean })
-  isSubmit = false;
+  isSubmit = false
 
   private _onButtonKeyDown(event: KeyboardEvent) {
     if (event.key === 'Enter' || event.key === ' ') {
-      this._onClick();
+      this._onClick()
     }
   }
 
   render() {
     return html`
+      <link rel="preload" href="../../../libs/icomoon/style.css" />
       <!-- Hello message -->
       <h1>Hello, ${this.name}!</h1>
-
+      <div>
+        <span class="icon icon-bike">
+          <span class="path1"></span>
+          <span class="path2"></span>
+          <span class="path3"></span>
+          <span class="path4"></span>
+          <span class="path5"></span>
+        </span>
+      </div>
       <!-- Button -->
       ${this.isSubmit
         ? html`
-            <input type="button" @click=${this._onClick} @keydown=${this._onButtonKeyDown} part="button" value="Click Count: ${this.count}" />
+            <input
+              type="button"
+              @click=${this._onClick}
+              @keydown=${this._onButtonKeyDown}
+              part="button"
+              value="Click Count: ${this.count}"
+            />
           `
         : html`
-            <button @click=${this._onClick} @keydown=${this._onButtonKeyDown} part="button" role="button" aria-label="Increment Count">
+            <button
+              @click=${this._onClick}
+              @keydown=${this._onButtonKeyDown}
+              part="button"
+              role="button"
+              aria-label="Increment Count"
+            >
               Click Count: ${this.count}
             </button>
           `}
 
-      <div id="dialog-description">Clicking this button will open a dialog to increment the count.</div>
+      <div id="dialog-description">
+        Clicking this button will open a dialog to increment the count.
+      </div>
 
       <!-- Slot content -->
       <slot></slot>
-    `;
+    `
   }
 
   private _onClick() {
-    this.count++;
+    this.count++
   }
 
   foo(): string {
-    return 'foo';
+    return 'foo'
   }
 }
 
 declare global {
   interface HTMLElementTagNameMap {
-    'my-element': MyElement;
+    'my-element': MyElement
   }
 }
