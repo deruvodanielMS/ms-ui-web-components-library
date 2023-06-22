1d3676869da6427f3b76f2f9fa1508eb0af8dcc3
DBorgognoni <dborgognoni@makingsense.com>
2023-06-22 10:28:08 -0300

    feat: input component

---
+++ src/components/atoms/input/ms-input.ts
@@ -0,0 +1,104 @@
+import { LitElement, html } from 'lit'
+import { customElement, property } from 'lit/decorators.js'
+
+import { WithTheme } from '~/mixins'
+
+import { msInputStyles } from './ms-input.styles'
+
+@customElement('ms-input')
+export class MSInput extends WithTheme(LitElement) {
+  static styles = msInputStyles
+
+  @property({ type: String })
+  value = ''
+
+  @property({ type: String })
+  label = ''
+
+  @property({ type: String })
+  message = 'Informative message'
+
+  @property({ type: String })
+  successLabel = 'Success message'
+
+  @property({ type: String })
+  errorLabel = 'Error message'
+
+  @property({ type: String })
+  placeholder = ''
+
+  @property({ type: String })
+  icon = ''
+
+  @property({ type: String })
+  iconState = ''
+
+  @property({ type: Boolean })
+  disabled = false
+
+  @property({ type: Boolean })
+  outline = false
+
+  @property({ type: Boolean })
+  success = false
+
+  @property({ type: Boolean })
+  error = false
+
+  render() {
+    return this.renderWithStyles(
+      html`
+        <div>
+          <ms-typography variant="p">${this.label}</ms-typography>
+          <div>
+            ${this.icon
+              ? html`
+                  <div class="icon-container">
+                    <i class="${this.icon}"></i>
+                  </div>
+                `
+              : ''}
+            <input
+              class=${this.success ? 'success' : this.error ? 'error' : ''}
+              ?error=${this.error}
+              ?success=${this.success}
+              ?disabled=${this.disabled}
+              type="text"
+              placeholder="${this.placeholder}"
+              .value=${this.value}
+              @input=${this.inputHandler}
+            />
+          </div>
+          <div>
+            ${this.iconState
+              ? html`
+                  <div class="icon-container">
+                    <i class="${this.icon}"></i>
+                  </div>
+                `
+              : ''}
+            <ms-typography variant="span"
+              >${this.success
+                ? this.successLabel
+                : this.error
+                ? this.errorLabel
+                : this.message}</ms-typography
+            >
+          </div>
+        </div>
+      `,
+    )
+  }
+
+  inputHandler(event: Event) {
+    this.dispatchEvent(
+      new CustomEvent('val-change', { detail: { value: event.composedPath()[0] } }),
+    )
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'ms-input': MSInput
+  }
+}
