677db52684b963fb7149161d830ca85b87ccc772
Cecilia Mermoz <cmermoz@makingsense.com>
2023-06-26 15:49:50 -0300

    #57: icon-button styes

--- src/components/atoms/icon-button/icon-button.stories.ts
+++ src/components/atoms/icon-button/icon-button.stories.ts
@@ -2,7 +2,8 @@ import type { Meta, StoryObj } from '@storybook/web-components'
 import { html } from 'lit'
 
 import { addIcon } from '~/assets/icons/add-icon'
-import './icon-button'
+
+import { sizesAllowed, variantAllowed } from './icon-button'
 
 export default {
   title: 'Icon Button',
@@ -13,11 +14,26 @@ export default {
     disabled: {
       control: { type: 'boolean' },
     },
+    variant: {
+      options: variantAllowed,
+      control: { type: 'select' },
+    },
+    size: {
+      options: sizesAllowed,
+      control: { type: 'select' },
+    },
   },
   render: (args) =>
     html`
-      <ms-icon-button .disabled=${args.disabled}>
+      <ms-icon-button
+        .disabled=${args.disabled}
+        ariaLabel="label for button"
+        .click=${args.handleClick}
+        .variant=${args.variant}
+        .size=${args.size}
+      >
         <svg
+          aria-hidden="true"
           width="20"
           height="20"
           viewBox="0 0 20 20"
@@ -34,5 +50,8 @@ export const IconButton: StoryObj = {
   name: 'MSIconButton',
   args: {
     disabled: false,
+    handleClick: () => alert('click'),
+    variant: 'contained',
+    size: 'small',
   },
 }
